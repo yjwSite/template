e3af821f38254fa5d7b33cd7379605c65a767172
shicaokuangmo <35943052@qq.com>
2017-12-02 17:20:40 +0800

    加入面包屑公共组件

---
+++ src/components/breadcrumb/BreadCrumb.vue
@@ -0,0 +1,27 @@
+/**
+* 作者 ：sunmeng
+*
+* 日期 ：2017/11/14
+*
+* 描述 ：公共面包屑组件
+*/
+<template>
+  <el-breadcrumb class="breadCrumb" separator-class="el-icon-arrow-right">
+    <el-breadcrumb-item
+      v-for="item in options.bread"
+      :to="{path : item.path}"
+      :label="item.label">
+      {{ item.label }}
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+<script>
+  export default {
+    name: 'BreadCrumb',
+    props: ['options']
+  }
+</script>
+
+<style>
+  @import "./bread.styl";
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -8,6 +8,7 @@
 <template>
   <el-container class="is-vertical">
     <HeaderBar :menuState="menuState"></HeaderBar>
+    <BreadCrumb :options="breadOptions" class="breadcreamb"></BreadCrumb>
     <el-main>
       <!--<Welcome></Welcome>-->
       <!--<MenuCount :menuCountData="menuCountData" style="margin-top: 20px;"></MenuCount>-->
@@ -18,14 +19,33 @@
 </template>
 <script>
   import HeaderBar from '../../components/header/Header.vue'
+  import BreadCrumb from '../../components/breadcrumb/BreadCrumb.vue'
   export default {
     name: 'index',
     components: {
-      HeaderBar
+      HeaderBar,
+      BreadCrumb
     },
     data () {
       return {
-        menuState: 'home'
+        menuState: 'home',
+        breadOptions:{
+          bread:[
+            {
+              label:"一级",
+              path:"/"
+            },{
+              label:"二级",
+              path:"/"
+            },{
+              label:"三级",
+              path:"/"
+            },{
+              label:"四级",
+              path:"/"
+            }
+          ]
+        }
       }
     },
     mounted () {
