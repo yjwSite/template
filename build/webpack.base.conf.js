a29e35a08e35b540725df6b348152fb5756a33e5
shicaokuangmo <35943052@qq.com>
2017-12-14 09:49:40 +0800

    兼容IE11的babel-ployfill加入

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -11,7 +11,9 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: ['babel-polyfill', './src/main.js'],
+    vendor: ['vue', 'vue-router', 'vuex'],
+    lib: ['element-ui']
   },
   output: {
     path: config.build.assetsRoot,
