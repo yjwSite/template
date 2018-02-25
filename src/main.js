83e22271663656ce328a58acd744b04522f59eb9
shicaokuangmo <35943052@qq.com>
2018-02-25 10:58:59 +0800

    moment加入

--- src/main.js
+++ src/main.js
@@ -3,11 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+// vuex
 import store from './store'
+
+// element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
 Vue.use(ElementUI)
+
+// swiper
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+
+// moment
+import moment from 'moment'
+moment.locale('zh-cn');
+Vue.prototype.$moment = moment
 
 /* eslint-disable no-new */
 new Vue({
