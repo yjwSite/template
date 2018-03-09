89f98cac3b59cc9d7b994da3f635a48665466d2e
shicaokuangmo <35943052@qq.com>
2018-03-09 11:28:09 +0800

    公共组件datalist加入

---
+++ src/components/datalist/DataList.vue
@@ -0,0 +1,45 @@
+/**
+* 作者 ：食草狂魔
+*
+* 日期 ：2018/1/9
+*
+* 描述 ：公共组件 - 数据列表
+*/
+<template>
+  <el-container class="datalistContainer is-vertical">
+    <el-row :class="options.containerClassName" :style="options.containerStyle">
+      <slot name="datalistItem" :data="item" :options="options" v-for="item in options.data"></slot>
+      <slot name="noData" v-if="Array.isArray(options.data)&&options.data.length<1"></slot>
+    </el-row>
+    <el-pagination v-if="options.pageInfo"
+                   @size-change="options.pageInfo.handleSizeChange"
+                   @current-change="options.pageInfo.handleCurrentChange"
+                   :current-page="options.pageInfo.currentPage"
+                   :page-sizes="options.pageInfo.pageSizes"
+                   :page-size="options.pageInfo.pageSize"
+                   :layout="options.pageInfo.layout"
+                   :total="options.pageInfo.total"
+                   :class="options.pageInfo.className"
+                   :prev-text="options.pageInfo.prevText"
+                   :next-text="options.pageInfo.nextText"
+                   class="pagination">
+      <span class="pageSlot" v-if="options.pageInfo.slotFromat&&options.pageInfo.slotFromat.length>0">
+        <template v-for="slotItem in options.pageInfo.slotFromat.split(',')">
+          <span class="pageInfo yema" v-if="slotItem==='yema1'">页数：{{options.pageInfo.currentPage}} / {{ Math.ceil(parseInt(options.pageInfo.total) / parseInt(options.pageInfo.pageSize))}}</span>
+          <span class="pageInfo pageof" v-if="slotItem==='pageof'">Page {{options.pageInfo.currentPage}} of {{ Math.ceil(parseInt(options.pageInfo.total) / parseInt(options.pageInfo.pageSize))}}</span>
+          <span class="pageInfo zonggong" v-if="slotItem==='zonggong'">总共 {{options.pageInfo.total}} 条数据</span>
+          <span v-if="slotItem==='space'">&nbsp;</span>
+        </template>
+      </span>
+    </el-pagination>
+  </el-container>
+</template>
+<script>
+  export default {
+    name: 'DataList',
+    props: ['options']
+  }
+</script>
+<style lang="less" scoped>
+  @import "datalist.less";
+</style>
---
+++ src/components/datalist/用法.vue
@@ -0,0 +1,104 @@
+/**
+* 作者 ：食草狂魔
+*
+* 日期 ：2018/3/8
+*
+* 描述 ：用法
+*/
+<template>
+  <el-main class="appContainer">
+    <el-row class="gray">
+      <MDataList :options="datalistOptions" class="container" style="margin-bottom: 15px;">
+        <div slot="datalistItem" slot-scope="props" class="dataBox">
+          <router-link :to="{path:`/front/appMarket/details?actId=2&detailId=${props.data.id}`}" target="_blank">
+            <div class="dataItemBox">
+              <div class="imageBox">
+                <img style="width: 60px;height: 60px" :src="props.data.iconId || 'static/images/serviceIcon2.png'">
+              </div>
+              <p class="title" :title="props.data.appName.length>8?props.data.appName:''">{{props.data.appName.substring(0,8)}}</p>
+              <p class="remark">{{props.data.introduction.length>49?props.data.introduction.substring(0,49)+'...':props.data.introduction}}</p>
+            </div>
+          </router-link>
+        </div>
+        <div slot="noData" class="noData"></div>
+      </MDataList>
+    </el-row>
+  </el-main>
+</template>
+<script>
+  import MDataList from '@/components/datalist/DataList.vue'
+  export default {
+    name: 'appMarket',
+    components: {
+      MDataList
+    },
+    data() {
+      return {
+        datalistOptions: {}
+      }
+    },
+    mounted: async function () {
+      let vm = this,
+        pageNo = 1,
+        pageSize = 15
+
+      /*
+      let [tagList, dataList] = await Promise.all([
+        labelsAPIs.labels({ type: 'appLabel', pageSize: 19, pageNumber: 1 }),
+        appsAPIs.apps({ appType: 0, isUsed: 1, pageSize: pageSize, pageNumber: pageNo})
+      ])
+      vm.initDataList(dataList.data.result.datas, pageNo, pageSize, parseInt(dataList.data.result.total))
+      */
+    },
+    methods: {
+      initDataList (dataList, pageNo, pageSize, total) {
+        let vm = this
+
+        // 改变pageSize事件
+        let handleSizeChangeFun = async (val) => {
+          vm.datalistOptions.pageInfo.pageSize = val
+        }
+
+        // 改变currentPage事件
+        let handleCurrentChangeFun = async (val) => {
+          vm.datalistOptions.pageInfo.currentPage = val
+
+          let keyword = vm.$refs.searchBar.$refs.keyword.$refs.input.value
+          let labels = vm.searchOptions.checkGroup.join(",")
+
+          let {data} = await appsAPIs.apps({
+            appType: 0,
+            label: labels=="0"?"":labels,
+            name: keyword,
+            pageSize: vm.datalistOptions.pageInfo.pageSize,
+            pageNumber: vm.datalistOptions.pageInfo.currentPage
+          })
+
+          vm.datalistOptions.data = data.result.datas
+        }
+
+        // 服务详情点击事件
+        let detailsOnClickFun = (item) => {
+//          console.log(item)
+          vm.$router.push({ path: '/front/serviceAbility/details' })
+        }
+
+        vm.datalistOptions = appMarketService({
+          dataList: dataList,
+          pageNo: pageNo,
+          pageSize: pageSize,
+          total: total
+        }).getDataListOptions({
+          handleSizeChangeFun,
+          handleCurrentChangeFun,
+          detailsOnClickFun
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+</style>
+
+
